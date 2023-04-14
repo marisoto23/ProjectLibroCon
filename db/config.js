adf1999c97acb7592df9bcd50984098f6eef3be4
Mariana Soto <86176976+marisoto23@users.noreply.github.com>
2023-04-13 18:31:33 -0600

    Config - Login
    - Se crea el archivo de conexion a la db
    - Se crea la interfaz de login

---
+++ db/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVCdhwnGXJ-tD-PxnYuoLodmTuyj139yc",
+  authDomain: "librocon-50df4.firebaseapp.com",
+  projectId: "librocon-50df4",
+  storageBucket: "librocon-50df4.appspot.com",
+  messagingSenderId: "253853222251",
+  appId: "1:253853222251:web:bb0de6e667a71232d0f82e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
